5e234c73b6f08f979e69f95eeb613dff61d71ded
William Kopaczewski <will.kopaczewski@gmail.com>
2025-08-01 03:18:53 -0400

    Now store thumbnails in database instead of server memory

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -31,7 +33,52 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const max_memory = 10 << 20
+	err = r.ParseMultipartForm(max_memory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	media_type := header.Header.Get("Content-Type")
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to read file", err)
+		return
+	}
+	meta_data, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video with given ID not found", err)
+		return
+	}
+
+	if meta_data.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You cannot upload a thumbnail for someone else", nil)
+		return
+	}
+
+	thumbnail_url := fmt.Sprintf("http://localhost:%s/api/thumbnails/%v", cfg.port, videoID)
+	meta_data.ThumbnailURL = &thumbnail_url
+
+	encoded_media_type := base64.StdEncoding.EncodeToString([]byte(media_type))
+	encoded_data := base64.StdEncoding.EncodeToString([]byte(data))
+	data_url := fmt.Sprintf("data:%s;base64,%s", encoded_media_type, encoded_data)
+	meta_data.ThumbnailURL = &data_url
+
+	err = cfg.db.UpdateVideo(meta_data)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error adding thumbnail to video", err)
+		return
+	}
+
+
+	respondWithJSON(w, http.StatusOK, meta_data)
 }
